44feedd0b00f4a938e8160a07198b9b660f9a290
Aidan Matrician <aidanmatrician@gmail.com>
2024-03-19 04:02:16 -0400

    2.1

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -2,6 +2,7 @@ import { Link, Route, Routes } from "react-router-dom";
 import Nav from "../nav";
 import Assignment3 from "./a3";
 import TodoList from "./a3/todos/TodoList";
+import Assignment4 from "./a4";
 
 function Labs() {
     return (
@@ -11,7 +12,7 @@ function Labs() {
         <Link to="/Labs/a4">Assignment 4</Link>
         <Routes>
           <Route path="/a3/*" element={<Assignment3 />} />
-          {/* <Route path="/a4" element={<Assignment4 />} /> */}
+          <Route path="a4" element={<Assignment4 />}/>
         </Routes>
         <TodoList/>
       </div>
